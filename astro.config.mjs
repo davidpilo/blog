e56f91f7d621fe9d84a138a1c1eeb17cc93a1dda
Jared Peña Ochoa <jared@jrdx0.dev>
2026-02-10 04:58:50 -0600

    prefijo agregado en links internos para funcionar en github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://jrdx0.github.io',
-	base: 'davidblog',
+	base: '/davidblog',
   integrations: [icon()]
 });
